366f06339a08b7de221d73e91c0f1eb0c558f5c2
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-30 18:28:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Generics2IComparableCompareTo.Entities;
+using Generics2IComparableCompareTo.Services;
+
+namespace Generics2IComparableCompareTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            Console.Write("Enter N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+            Console.WriteLine("Max: " + max);
+        }
+    }
+}
